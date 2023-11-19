fe57599f3e880de09e3d13c585495b7912d5b6fc
riley <alkalinarwhal@gmail.com>
2023-11-18 20:32:18 -0800

    more additions

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,70 @@
  * App.tsx
  * 
  * lets burn everything for now
+ * 
+ * useState for dynamic changes
+ * useEffect for listening to keypresses "side effects"
  */
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 /**
+ * @function Square()
  * 
- * @returns our App
+ * squares make up our game grid
+ * they can either be empty, have a letter, or have something else (tbd)
+ * 
+ * 
+ * @return dynamic square css element
  */
-export default function App ()
+const Square = () =>
 {
-    const [value, setValue] = useState(String(""));
+    /**
+     * @var filled whether or not this square has something inside it
+     * @var letter the inner letter of the square, provided it is filled
+     */
+    const [filled, setFilled] = useState(false);
+    const [letter, setLetter] = useState("");
 
+    /*
+        behavior of square, for now when mouse enters
+        but it will be something else
+    */
     function handleMouseEnter ()
     {
-        var head = document.getElementById("top");
-        var middle = document.getElementById("middle")
-        var grid = document.getElementById("grid");
-
-        if (grid && middle)
+        if (filled)
         {
-            var height = grid.style.getPropertyValue('height');
-            if (height)
-            {
-                setValue("" + height);
-            }
+            setFilled(false);
+            setLetter("");
         }
         else
         {
-            setValue("lol!!");
+            setFilled(true);
+            setLetter(String.fromCharCode(Math.floor(Math.random()*26)+65));
         }
+    }
+
+
+    return (
+        <div className="square" onMouseEnter={handleMouseEnter}>
+            {letter}
+        </div>
+    )
+}
+
+/**
+ * @function App
+ * 
+ * @returns our app in all of its glory
+ */
+const App = () =>
+{
+    const [value, setValue] = useState(String(""));
+    var head = document.getElementById("top");
+
+    const handleMouseEnter = () =>
+    {
+        setValue("lol!");
+        
         
         // ensure not null
         if (head)
@@ -40,14 +74,11 @@ export default function App ()
             // add/remove classes
             // head.classList.add("something");
         }
-        
     }
 
-    function handleMouseLeave ()
+    const handleMouseLeave = () =>
     {
-        // setValue(value + "\b\b\b\b");
-
-        var head = document.getElementById("top");
+        setValue("nooo!!");
         
         // ensure not null
         if (head)
@@ -56,59 +87,97 @@ export default function App ()
         }
     }
 
+
+    /**
+     * @function handleKeyDown()
+     * @param event keypress
+     * 
+     * we extract the pressed `key` and set `value` to it
+     */
+    const handleKeyDown = (event: any) =>
+    {
+        setValue(event.key);
+    }
+
+    /**
+     * ok trying to grasp understanding this,
+     * @function useEffect()
+     * 
+     * we import this function from `React` at the top of this document,
+     * and we call it here
+     * 
+     * we pass in a CALLBACK FUNCTION as the first argument,
+     * which contains the code that we want to execute when this function is called
+     * 
+     * the second argument is an EMPTY DEPENDENCY ARRAY
+     * typically, when a dependency in the array changes, it calls this function
+     * 
+     * since the dependency array is EMPTY, this function is only called ONCE, when the component initially mounts
+     */
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+
+        // clean up event listener on component dismount (avoid memory leak)
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
     return (
         <>
             <div id="top" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                psychic fiesta
+                psychic fiesta {value}
             </div>
 
             <div id="middle">
                 <div id="grid">
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                     
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
 
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
 
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
 
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
-                    <div className="square"></div>
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
+                    <Square />
                 </div>
             </div>
-
-            
         </>
     );
 }
+
+
+// declare our export
+export default App;
